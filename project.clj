30c6711f6480f5d3335338e1351cf7b4e5a5a9d3
rod <rod@pu-gh.com>
2012-04-07 16:09:06 +0100

    moved midje to dev dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 
-(defproject clj-php "0.0.2"
+(defproject clj-php "0.0.3"
   :description "Clojure to PHP compiler"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [midje "1.3.1"]
-                 [com.stuartsierra/lazytest "1.2.3"]]
-  :dev-dependencies [[lein-midje "1.0.8"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[com.stuartsierra/lazytest "1.2.3"]
+                     [lein-midje "1.0.8"]
+                     [midje "1.3.1"]]
   :repositories {"stuart" "http://stuartsierra.com/maven2"}
   :main clj-php.core
   :uberjar-name "clj-php.jar")

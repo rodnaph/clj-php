301c0af292de39585f02dc1552f933d5dd2d9586
rod <rod@pu-gh.com>
2012-04-07 16:13:03 +0100

    trying excluding clojure/core from midje

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 
-(defproject clj-php "0.0.3"
+(defproject clj-php "0.0.4"
   :description "Clojure to PHP compiler"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[com.stuartsierra/lazytest "1.2.3"]
                      [lein-midje "1.0.8"]
-                     [midje "1.3.1"]]
+                     [midje "1.3.1" :exclusions [org.clojure/clojure]]]
   :repositories {"stuart" "http://stuartsierra.com/maven2"}
   :main clj-php.core
   :uberjar-name "clj-php.jar")
